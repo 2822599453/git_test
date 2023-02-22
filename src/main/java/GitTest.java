57fdb888eb66fec0137c313c3ce1daaed62594cc
Chen <2822599453@qq.com>
2023-02-22 22:38:24 +0800

    IDEA second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("1111");
+        System.out.println("2222");
     }
 }
