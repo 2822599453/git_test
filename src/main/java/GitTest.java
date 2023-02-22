f67482adfb68c9055689a8dfa54edcc021857fba
Chen <2822599453@qq.com>
2023-02-22 23:05:42 +0800

    IDEA hox-fix commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("1111");
         System.out.println("2222");
+        System.out.println("33333 hot-fix");
     }
 }
